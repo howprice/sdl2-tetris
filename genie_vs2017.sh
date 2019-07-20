03ba1f75b60ec8164cac387808aa905e690b4adc
howprice <howardjohnprice@gmail.com>
2019-07-20 19:08:29 +0100

    Travis Windows WIP

--- genie_vs2017.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    ./tools/bin/linux/genie vs2017
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # Mac OSX
-    ./tools/bin/macosx/genie vs2017
-fi
-
